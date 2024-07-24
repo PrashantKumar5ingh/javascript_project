cbb828f00ecc615e88a2231891a356a2a3222d42
PrashantKumar <prashantkumar.singh@avxpll447.appviewx.com>
2024-07-24 23:14:34 +0530

    scope of var in different way

--- index.js
+++ index.js
@@ -11,7 +11,29 @@ console.log("Hello world")
 // console.log(x);
 
 // var multiple definition
-var x = 10;
-console.log(x);
-var x = "Manoj";
-console.log(x);
+// var x = 10;
+// console.log(x);
+// var x = "Manoj";
+// console.log(x);
+
+// var scope check
+
+// global scope check - pass
+
+var a = 10
+{
+    console.log(a);
+}
+
+// usage before declaration check - pass
+
+b = 10;
+console.log(b);
+var b;
+
+//usage before initialisation and declaration check- pass with c as undefined
+
+console.log(c);
+var c = 10;
+
+//
